ff8dc1e1bee70595c385033b97f658813d044e90
Nikasa <nikasa.1889@gmail.com>
2017-04-06 13:28:13 +0200

    Finish mqueue.c

--- mqueue/common.h
+++ mqueue/common.h
@@ -3,10 +3,10 @@
 
 #define EQ_NAME     "/equeue"
 #define EQ_MAX_SIZE    16384
-#define EQ_MAX_MSG     20
+#define EQ_MAX_MSG     5
 
 #define RQ_PREFIX   "/rqueue"
-#define RQ_MAX_SIZE 10
+#define RQ_MAX_SIZE 1
 #define RQ_MAX_MSG 1
 
 #define RQ_NAME_MAX 30
@@ -16,11 +16,12 @@ typedef struct Req
 {
   char rq_name[RQ_NAME_MAX];
   char program[EQ_MAX_SIZE-20];
-}
+} Req;
+
 typedef struct Res
 {
   float result;
-}
+} Res;
 
 #define CHECK(x) \
     do { \
--- mqueue/mqueue.c
+++ mqueue/mqueue.c
@@ -9,11 +9,26 @@
 #include "common.h"
 
 mqd_t EQ;
-
 mqd_t RQ;
 char RQ_NAME[RQ_NAME_MAX];
 char eq_buffer[EQ_MAX_SIZE + 1];
 char rq_buffer[RQ_MAX_SIZE + 1];
+
+//Prototypes
+int eq_open_write();
+int eq_open_read();
+int eq_send(Req *req);
+struct Req eq_receive();
+
+int rq_open_write();
+int rq_open_read();
+int rq_send(Res *res);
+struct Res rq_receive();
+
+
+float requestEvaluator(char * program, int length){
+void evaluateForever (float (*evaluateFunc)(char *program)){
+
 int eq_open_write(){
   EQ = mq_open(EQ_NAME, O_WRONLY);
   if ((mqd_t)-1 == EQ){
@@ -23,18 +38,39 @@ int eq_open_write(){
   }
 }
 
-int eq_send(char *buffer, int msg_len){
-  int result;
+int eq_open_read(){
+  struct mq_attr EQ_ATTR;
+  EQ_ATTR.mq_flags = 0;
+  EQ_ATTR.mq_maxmsg = EQ_MAX_MSG;
+  EQ_ATTR.mq_msgsize = EQ_MAX_SIZE;
+  EQ = mq_open(EQ_NAME, O_CREAT | O_RDONLY, 0644, &EQ_ATTR);
+
+  if ((mqd_t) -1 == EQ){
+    return -1;
+  } else {
+    return 0;
+  }
+}
+int eq_send(Req *req){
+  int ret;
   //char str[msg_len + RQ_NAME_MAX + 1];
   //strcpy(str, RQ_NAME);
   //strcat(str, ",");
   //strcat()
-  result = mq_send(EQ, buffer, msg_len, 0);
-  if (result >= 0){
+  ret = mq_send(EQ, (const char *) &req, sizeof(req), 0);
+  if (ret >= 0){
     return 0;
   } else {
     return -1;
   }
+}
+
+Req eq_receive(){
+  Req request;
+  ssize_t bytes_read;
+  bytes_read = mq_receive(EQ, (char *) &request, sizeof(request), NULL);
+  //TODO: what if bytes_read < 0?
+  return request;
 }
 
 int rq_open_read(){
@@ -51,28 +87,71 @@ int rq_open_read(){
   }
 }
 
-float rq_read(){
+
+Res rq_receive(){
   ssize_t bytes_read;
-  float result;
-  bytes_read = mq_receive(RQ, rq_buffer, RQ_MAX_SIZE, NULL);
-  if (bytes_read >= 0){
-    result = *(float*)&rq_buffer;
-    return result;
+  Res response;
+  bytes_read = mq_receive(RQ, (char *) &response, RQ_MAX_SIZE, NULL);
+  return response;
+}
+
+int rq_send(Res *res){
+  int ret;
+  ret = mq_send(RQ, (const char *) &res, sizeof(res), 0);
+  if (ret >= 0){
+    return 0;
   } else {
     return -1;
   }
 }
 
-float eq_evaluate(char *buffer, int msg_len){
+int initMyRQName(){
+  sprintf(RQ_NAME, "%s%ld",RQ_PREFIX,(long)getpid());
+  return 0;
+}
+
+Req createRequest(char * program, int length){
+  Req request;
+  strcpy(request.rq_name, RQ_NAME);
+  strcpy(request.program, program);
+  return request;
+}
+
+Res createResponse(float result){
+  Res response;
+  response.result = result;
+  return response;
+}
+
+float requestEvaluator(char * program, int length){
   int ret;
   float result;
-  sprintf(RQ_NAME, "%s%ld",RQ_PREFIX,(long)getpid());
+  Req request;
+  Res response;
+  initMyRQName();
   ret = eq_open_write();
-  ret = eq_send(buffer, msg_len);
+  request = createRequest(program, length);
+  ret = eq_send(&request);
   ret = rq_open_read();
-  result = rq_read();
+  response = rq_receive();
   mq_close(RQ);
   mq_unlink(RQ_NAME);
   mq_close(EQ);
-  return result;
+  return response.result;
+}
+
+void evaluateForever (float (*evaluateFunc)(char *program)){
+  Req request;
+  Res response;
+  int ret;
+  float result;
+  ret =  eq_open_read();
+  while(1){
+    request = eq_receive();
+    strcpy(RQ_NAME, request.rq_name);
+    rq_open_write();
+    result = (*evaluateFunc) (request.program);
+    response = createResponse(result);
+    ret = rq_send(&response);    
+  }
 }
